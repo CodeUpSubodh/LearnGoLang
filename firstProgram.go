1457eeb6ede1aaf1d1a8686e4f3652d204632a73
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-05-25 18:37:49 +0530

    Slice Range functions

--- firstProgram.go
+++ firstProgram.go
@@ -15,8 +15,11 @@ func main() {
 	card5 := newType()
 	card6 := deck{"UNO Reverse", "UNO Skip"}
 
-	card6.pritn()
+	card6.print()
 
+	fruitSlice := newSliceTut()
+
+	fmt.Println(fruitSlice)
 	fmt.Println(anotherFilefunction())
 	fmt.Println(card5)
 	fmt.Println(card4)
@@ -42,6 +45,15 @@ func newSlice() []string {
 	return anotherNewSlice
 }
 
+func newSliceTut() []string {
+	fruits := []string{"Apple", "Banana", "Grapes"}
+	fmt.Println(fruits[0])
+	fmt.Println(fruits[0:2]) //This means from 0 to excluding 2
+	fmt.Println(fruits[:2])
+	fmt.Println(fruits[2:])
+	return fruits
+}
+
 type deck []string
 
 // Custome Type Declaration
@@ -52,9 +64,10 @@ func newType() deck {
 
 }
 
-// Receiver Function
+// Receiver Function d
+// This Working is same like how in OOPs related we create a class and use keywords like self and this to create the funcation that are related to that class
 
-func (d deck) pritn() deck {
+func (d deck) print() deck {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
