feaba1d24d2c55bf3f333f08032c9c74e42b3d6c
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-05-31 15:18:35 +0530

    Taking input

--- OddEvenProject/OddEvenProgram.go
+++ OddEvenProject/OddEvenProgram.go
@@ -5,8 +5,19 @@ import "fmt"
 type intslice []int
 
 func main() {
+	var length int
 
-	intslice := intslice{1, 58, 89, 24, 67, 61}
+	fmt.Print("Lenght of slice ")
+	fmt.Scan(&length)
+
+	intslice := intslice{length}
+
+	for i := 0; i < length; i++ {
+		var value int
+		fmt.Scan(&value)
+		intslice = append(intslice, value)
+	}
+
 	fmt.Println(intslice)
 	for _, value := range intslice {
 		if value%2 == 0 {
