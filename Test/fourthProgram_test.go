a8b75d361b54596c04f8c1c92c147a4e975263fa
Subodh <subodh@Subodhs-MacBook-Air.local>
2025-05-31 11:38:29 +0530

    Assert in Go

--- Test/fourthProgram_test.go
+++ Test/fourthProgram_test.go
@@ -7,12 +7,17 @@ import (
 
 // before writing any test case in go run command go mod init {directory name like LearnGoLoand}- This will create a file of go.mod after which you can execute go test
 func TestDivide(t *testing.T) {
-	// a, b := 2.4, 5.2 //Success Case
-	a, b := 0.0, 0.0 //Failure Case
+	a, b := 2.4, 5.2 //Success Case
+	// a, b := 0.0, 0.0 //Failure Case
 	value, err := divide(a, b)
 	fmt.Println(value)
 	if err != nil {
 		t.Errorf("0 cannot be the denominator %v", err) //to make use of dynamic value we use %v in go
+
+	}
+
+	if value != a/b {
+		t.Errorf("Getting Invalid Values") // These if are acting as assert but in go it does not know how any Testcases are passed or faield
 
 	}
 
