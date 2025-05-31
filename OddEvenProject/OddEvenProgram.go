8d8fe2e79b4b9287e9492ddb18564c1bd5ee145f
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-05-31 14:27:27 +0530

    Odd Even Program

---
+++ OddEvenProject/OddEvenProgram.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type intslice []int
+
+func main() {
+
+	intslice := intslice{1, 58, 89, 24, 67, 61}
+	fmt.Println(intslice)
+	for _, value := range intslice {
+		if value%2 == 0 {
+			fmt.Println("Even", value, "Value")
+			fmt.Printf("Even %v Value\n", value)
+
+		} else {
+			fmt.Println("Odd Value", value)
+		}
+	}
+}
