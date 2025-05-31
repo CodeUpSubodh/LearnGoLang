2f1f1afef28027de62ef7dbecb2de314558ea987
Subodh <subodh@Subodhs-MacBook-Air.local>
2025-05-31 16:27:52 +0530

    defaults and printing struct

--- structs/structs.go
+++ structs/structs.go
@@ -12,6 +12,10 @@ type person struct {
 func main() {
 	// subodh := person{"Subodh", "Srivastava"} //Depends on assigning the value of the basis of var defined in struct
 	subodh := person{firstName: "Subodh", lastName: "Srivastava"} //Same work but different syntax
-	fmt.Println(subodh)
+	var codeupsubodh person
+	//Go assign default for strings its "" int- 0 float - 0 and bool - false
+	fmt.Println(codeupsubodh) //empty struct
+	fmt.Println(subodh)       //{Subodh Srivastava}
+	fmt.Printf("%+v", subodh) //{firstName:Subodh lastName:Srivastava}%
 
 }
