bd7b8f1afd1500cbc5f56fccb4a2e27a8377eccd
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-06-01 12:25:53 +0530

    Changing directlry name and adding pointer

---
+++ structs_and_pointers/structs.go
@@ -0,0 +1,21 @@
+// stuct in go means collection of related properties
+
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	// subodh := person{"Subodh", "Srivastava"} //Depends on assigning the value of the basis of var defined in struct
+	subodh := person{firstName: "Subodh", lastName: "Srivastava"} //Same work but different syntax
+	var codeupsubodh person
+	//Go assign default for strings its "" int- 0 float - 0 and bool - false
+	fmt.Println(codeupsubodh) //empty struct
+	fmt.Println(subodh)       //{Subodh Srivastava}
+	fmt.Printf("%+v", subodh) //{firstName:Subodh lastName:Srivastava}%
+
+}
