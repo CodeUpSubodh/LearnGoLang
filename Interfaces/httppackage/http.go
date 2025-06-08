6097aa09e24f3e0506c10677bbb805c5c36d933c
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-06-09 01:10:12 +0530

    Implemented custom log Writter

--- Interfaces/httppackage/http.go
+++ Interfaces/httppackage/http.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {
+}
 
 func main() {
 	resp, err := http.Get("https://google.com")
@@ -12,8 +16,19 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	// fmt.Println(resp)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// io.Copy(os.Stdout, resp.Body) //Do the equivalent of above four lines including print
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Printed this many bytes", len(bs))
+	return len(bs), nil
 }
