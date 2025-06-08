e1662730188f380adfcb5fd4baad509715571b97
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-06-09 01:29:02 +0530

    shapes assignment

---
+++ AssignmentInterfaces/assignment.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	base   float64
+	hieght float64
+}
+
+type shapes interface {
+	area() float64
+}
+
+func (s square) area() float64 {
+	var area float64
+	area = s.sideLength * s.sideLength
+	return area
+}
+
+func (t triangle) area() float64 {
+	var area float64
+	area = t.base * t.hieght * 1 / 2
+	return area
+}
+
+func printArea(s shapes) {
+	fmt.Println(s.area())
+}
+
+func main() {
+	square := square{2.5}
+	triangle := triangle{3, 4}
+	printArea(square)
+	printArea(triangle)
+
+}
