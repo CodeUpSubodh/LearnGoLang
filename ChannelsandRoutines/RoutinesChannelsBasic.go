eb75c288b507f29f74a2084b4d378abdbd769561
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-06-12 00:37:49 +0530

    Add Channel and Routines Basics

---
+++ ChannelsandRoutines/RoutinesChannelsBasic.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{"http://google.com", "http://facebook.com", "http://stackoverflow.com", "http://golang.org", "http://amazon.com"}
+	c := make(chan string) //Creating Channel
+
+	for _, link := range links {
+		//checkLink(link) //There is delay at the time of execution because go is waiting for response and then proceeding to other execution process
+		go checkLink(link, c) //Creates Go Routine and passing channel reference in function
+	}
+
+	// fmt.Println(<-c) //Reciving a value through channel even after this implementaion we will recive data from only one go routine
+	// fmt.Println(<-c)
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "Might be down!")
+		c <- "Might be Down" //Sending data through channel
+	}
+	fmt.Println(link, "is up!")
+	c <- "Its Up"
+}
